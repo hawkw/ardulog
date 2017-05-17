c4da015b0c0051563e83766b73fba25e3307d84f
Eliza Weisman <hi@hawkweisman.me>
2017-05-17 11:21:41 -0400

    chore: add empty project files

---
+++ src/ArduLog.h
@@ -0,0 +1,18 @@
+/*
+    Arduino logging macros
+
+    by Eliza Weisman
+    eliza@elizas.website
+
+ */
+
+#ifndef ArduLog_h
+#define ArduLog_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif
+
+#endif
